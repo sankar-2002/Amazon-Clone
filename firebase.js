5968c14eb48d004a0269e3858f7d8de047f3e02e
Sankaranarayanan Sengunther <shankar.muthusamy4@gmail.com>
2021-08-17 16:38:01 +0530

    cart-page complete and firebase added

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBNyrCI4KpdsXSPoZBjSQ1mgiQXVm0oQiY",
+    authDomain: "clone-d2873.firebaseapp.com",
+    projectId: "clone-d2873",
+    storageBucket: "clone-d2873.appspot.com",
+    messagingSenderId: "1018841611955",
+    appId: "1:1018841611955:web:6e66944685908c7e0fbac6",
+    measurementId: "G-22TDHHERR0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
